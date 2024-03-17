58e7e02182350b08cd9b3003a330d39e130911ee
Aabis Ahmed Hassan <aabisahmedhassan3@gmail.com>
2024-03-18 04:10:23 +0500

    Done with Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_firebase/responsiveness/layout_determiner.dart';
 import 'package:instagram_clone_firebase/responsiveness/mobile_layout.dart';
 import 'package:instagram_clone_firebase/responsiveness/web_layout.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
@@ -18,7 +24,9 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.black,
       ),
       home: LayoutDeterminer(
-          WebLayout: WebLayout(), MobileLayout: MobileLayout(),),
+        WebLayout: WebLayout(),
+        MobileLayout: MobileLayout(),
+      ),
     );
   }
 }
